7bc5504d7b05605f9308634f052dad9e12e36644
William Lima <107055555+Emboava2022@users.noreply.github.com>
2023-01-02 14:39:11 -0300

    Update desafio.kt

--- desafio.kt
+++ desafio.kt
@@ -1,21 +1,39 @@
 // [Template no Kotlin Playground](https://pl.kotl.in/WcteahpyN)
+	
+	enum class Nivel { BASICO, INTERMEDIARIO, AVANÇADO }
+	
+	data class Aluno(val nome: String)
+	
+	data class ConteudoEducacional(val nome: String, val duracao: Int = 60)
+	
+	data class Formacao(val nome: String, val nivel: Nivel, val conteudos: List<ConteudoEducacional>) {
+	
+	val inscritos = mutableListOf<Aluno>()
+	
+	fun matricular(aluno: Aluno) {
+	    inscritos.add(aluno)
+	}
+	
+	fun showInscritos(): MutableList<Aluno> {
+	    return inscritos
+	    }
+	}
+	
+	fun main() {
+	    val aluno_1 = Aluno("Pedro")
+	    val aluno_2 = Aluno("André")
+	    val conteudos: List<ConteudoEducacional> = listOf(ConteudoEducacional("Linguagem Kotlin"), ConteudoEducacional("Linguagem de Programação Kotlin", 60))
+	    val formacao = Formacao("Programacao", Nivel.BASICO, conteudos)
+	
+	formacao.matricular(aluno_1)
+	formacao.matricular(aluno_2)
+	
+	println("Alunos matriculados: ${aluno_1.nome}, ${aluno_2.nome}")
+	println()
+	println("Conteudos: $conteudos")
+	println()
+	println("Formacao: ${formacao.toString()}")
+	println()
+	println("Inscritos da formacao: ${formacao.showInscritos()}")
+	}
 
-enum class Nivel { BASICO, INTERMEDIARIO, DIFICIL }
-
-class Usuario
-
-data class ConteudoEducacional(var nome: String, val duracao: Int = 60)
-
-data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>) {
-
-    val inscritos = mutableListOf<Usuario>()
-    
-    fun matricular(usuario: Usuario) {
-        TODO("Utilize o parâmetro $usuario para simular uma matrícula (usar a lista de $inscritos).")
-    }
-}
-
-fun main() {
-    TODO("Analise as classes modeladas para este domínio de aplicação e pense em formas de evoluí-las.")
-    TODO("Simule alguns cenários de teste. Para isso, crie alguns objetos usando as classes em questão.")
-}
